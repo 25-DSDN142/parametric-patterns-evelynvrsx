a31647d78b5d8fb37eb187a4e749d2f69381c1c2
Evelyn <evelynveronica1412@gmail.com>
2025-07-29 11:39:29 +1200

    added random colors and if statement

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,14 +2,17 @@
 let flowerX = 100;
 let flowerY = 100;
 let flowerDiameter = 90;
-let petalNum = 7;
+let petalNum = 10;
+let outsidePetalNum = 5;
+let numberOfFlowers = 5;
+let colors = [];
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   //pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -29,22 +32,55 @@ function my_symbol() {
   let yellow = color(254,214,123);
   let blue = color(173, 216, 230);
 
-  let defaultColor = gold;
+  // Push colors to the array
+  colors.push(gold);
+  colors.push(pink);
+  colors.push(red);
+  colors.push(yellow);
+  colors.push(blue);
+
+  let defaultColor = random(colors);
 
   // flower petal 
   fill(defaultColor);
   noStroke();
-  flowerPetals(flowerX, flowerY, petalNum, defaultColor);
+  insideFlowerPetals(flowerX, flowerY, petalNum, defaultColor);
 
   // middle of flower
   strokeWeight(2);
-  stroke(32,31,34);
+  stroke(32,31,34); //background color
   fill(defaultColor);
   ellipse(flowerX, flowerY, flowerDiameter/5, flowerDiameter/5);
+  //middleOfFlower(flowerX, flowerY, flowerDiameter);
+
+  // outside flower petal
+  strokeWeight(2);
+  stroke(defaultColor);
+  noFill();
+  outsideFlowerPetals(flowerX, flowerY, outsidePetalNum, defaultColor);
+
+  // how many motifs are in the 200x200 screen
+  if (numberOfFlowers < 7) {
+    flowerDiameter = 40;
+    strokeWeight(1);
+
+    for (let i = 0; i < numberOfFlowers; i++) {
+      flowerX = floor(random(20, 190));
+      flowerY = floor(random(20, 180));
+      fill(defaultColor);
+      insideFlowerPetals((flowerX, flowerY, petalNum, defaultColor));
+
+      //middleOfFlower(flowerX, flowerY, flowerDiameter);
+      outsideFlowerPetals(flowerX, flowerY, outsidePetalNum, defaultColor);
+    }
+  }
+  else {
+    
+  }
 }
 
 // make flower petals 
-function flowerPetals(midX, midY, petalNumber, color) {
+function insideFlowerPetals(midX, midY, petalNumber, color) {
   let petalRotation = 360/petalNumber;
   
   for (let i = 0; i < petalNumber; i++) {
@@ -55,6 +91,29 @@ function flowerPetals(midX, midY, petalNumber, color) {
       translate(midX, midY);
       rotate(petalRotation*i);
       ellipse(0, 0-(flowerDiameter/5), flowerDiameter/6, flowerDiameter/3);
+    pop();
+  }
+}
+
+// make middle of flower
+function middleOfFlower(midX, midY) {
+  strokeWeight(2);
+  stroke(32,31,34); //background color
+  fill(defaultColor);
+  ellipse(midX, midY, flowerDiameter/5, flowerDiameter/5);
+}
+
+// make outside flower petals
+function outsideFlowerPetals(midX, midY, outsidePetalNumber) {
+  let petalRotation = 360/outsidePetalNumber;
+  
+  for (let i = 0; i < 5; i++) {
+   push();
+      translate(midX, midY);
+      rotate(petalRotation*i);
+
+      arc(0, 0-(flowerDiameter/5), flowerDiameter, flowerDiameter/2, 45, 360);
+
     pop();
   }
 }
